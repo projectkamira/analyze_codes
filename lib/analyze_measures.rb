bea736be0e0aedda4759b217e135a18e5ffae146
Marc Hadley <mhadley@mitre.org>
2013-04-17 15:40:20 -0400

    Measure population level reporting on value set intersections

--- lib/analyze_measures.rb
+++ lib/analyze_measures.rb
@@ -15,23 +15,106 @@ module AnalyzeMeasures
         :nqfId => measure.nqf_id,
         :cmsId => measure['cms_id']
       }
-      measure.oids.each do |value_set|
-        if value_set_results[value_set]
-          intersection[:totalCodes]+=value_set_results[value_set][:totalCodes]
-          intersection[:codesFound]+=value_set_results[value_set][:codesFound]
+      AnalyzeMeasures.extract_code_counts!(intersection, measure.oids, value_set_results)
+      [:population, :denominator, :numerator, :exclusions, :exceptions].each do |pop|
+        if measure[pop.to_s]
+          pop_intersection = {
+            :totalCodes => 0,
+            :codesFound => 0,
+          }
+          pop_value_sets = AnalyzeMeasures.extract_population_value_sets(measure[pop.to_s])
+          AnalyzeMeasures.extract_code_counts!(pop_intersection, pop_value_sets, value_set_results)
+          if pop_intersection[:totalCodes] > 0
+            # ignore empty populations, e.g. a denom that just refs the ipp
+            intersection[pop] = pop_intersection
+          end
         end
       end
-      intersection[:percentFound] = 100.0*intersection[:codesFound]/intersection[:totalCodes]
       result[measure.id] = intersection
     end
     result
   end
   
+  def self.process_item!(item, value_sets)
+    if item['conjunction']
+      item['items'].each do |child_item|
+        AnalyzeMeasures.process_item!(child_item, value_sets)
+      end
+    else
+      if item['code_list_id']
+        value_sets.push item['code_list_id']
+      end
+      if item['temporal_references']
+        item['temporal_references'].each do |ref|
+          if ref['reference']
+            AnalyzeMeasures.process_item!(ref['reference'], value_sets)
+          end
+        end
+      end
+    end
+  end
+  
+  def self.extract_population_value_sets(population)
+    value_sets = []
+    AnalyzeMeasures.process_item!(population, value_sets)
+    value_sets
+  end
+  
+  def self.extract_code_counts!(intersection, value_sets, value_set_results)
+    intersection[:valueSetCount] = value_sets.length
+    redundant_value_sets = value_sets.select {|value_set| !value_set_results[value_set] || value_set_results[value_set][:codesFound] == 0}
+    intersection[:redundantValueSetCount] = redundant_value_sets.length
+    intersection[:redundantValueSets] = redundant_value_sets
+    value_sets.each do |value_set|
+      if value_set_results[value_set]
+        intersection[:totalCodes]+=value_set_results[value_set][:totalCodes]
+        intersection[:codesFound]+=value_set_results[value_set][:codesFound]
+      end
+    end
+    if intersection[:totalCodes] > 0
+      intersection[:percentFound] = 100.0*intersection[:codesFound]/intersection[:totalCodes]
+    else
+      intersection[:percentFound] = 0.0
+    end
+  end
+  
   def self.as_2d_array(result)
-    arr = [[:measureId, :nqfId, :cmsId, :totalCodes, :codesFound, :percentFound, :measureName]]
-    arr.concat(result.collect do |key, value|
-      [key, value[arr[0][1]], value[arr[0][2]], value[arr[0][3]], value[arr[0][4]], value[arr[0][5]], value[arr[0][6]]]
-    end.to_a)
+    arr = [[:measureId, :measureName, :population, :nqfId, :cmsId, :totalCodes, :codesFound, :percentFound, :valueSetCount, :redundantValueSetCount]]
+    measure_summary_rows = result.collect do |measure_id, measure_result|
+      [
+        measure_id,
+        measure_result[arr[0][1]],
+        :all,
+        measure_result[arr[0][3]],
+        measure_result[arr[0][4]],
+        measure_result[arr[0][5]],
+        measure_result[arr[0][6]],
+        measure_result[arr[0][7]],
+        measure_result[arr[0][8]],
+        measure_result[arr[0][9]]
+      ]
+    end.to_a
+    arr.concat(measure_summary_rows)
+    result.each do |measure_id, measure_result|
+      [:population, :denominator, :numerator, :exclusions, :exceptions].each do |pop|
+        if measure_result[pop]
+          arr.insert(-1, [
+              measure_id,
+              measure_result[arr[0][1]],
+              pop,
+              measure_result[arr[0][3]],
+              measure_result[arr[0][4]],
+              measure_result[pop][arr[0][5]],
+              measure_result[pop][arr[0][6]],
+              measure_result[pop][arr[0][7]],
+              measure_result[pop][arr[0][8]],
+              measure_result[pop][arr[0][9]]
+            ]
+          )
+        end
+      end
+    end
+    arr
   end
   
   def self.load_measures(dir)
